a93857f9d4dca89339a47e7d7f10221ee79f6512
leo2n <ttxs.an@gmail.com>
2020-06-30 19:56:48 +0800

    定义了请求参数的解析结构体和解析方法

--- req_struct.go
+++ req_struct.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type BuyReq struct {
 	UserId     string `json:"userId"`
 	ProductId  string `json:"productId"`
-	ProductNum int    `json:"productNum"`
+	PurchaseNum int    `json:"purchaseNum"`
+}
+
+func decodeBuyReq(buyReq io.ReadCloser) (*BuyReq, error){
+	b := new(BuyReq)
+	err := json.NewDecoder(buyReq).Decode(b)
+	if err!=nil {
+		return new(BuyReq), err
+	}
+	return b, nil
 }
