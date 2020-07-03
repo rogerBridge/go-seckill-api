96fa9d8a4c6334ce31e9e90a26b71fdccc74e5d5
leo2n <ttxs.an@gmail.com>
2020-07-03 18:54:44 +0800

    库存必须大于等于单个用户允许购买的最大值

--- global_var.go
+++ global_var.go
@@ -5,5 +5,5 @@ var (
 	networkType = "tcp"
 	address = "localhost:6379"
 	passwd = "hello"
-	limitNum = 2 // 限制每个用户可以购买的相同productId的商品的数量
+	limitNum = 2 // 限制每个用户可以购买的相同productId的商品的数量, limitNum 必须小于等于商品实际的库存
 )
