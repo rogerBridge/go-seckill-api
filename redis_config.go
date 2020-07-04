5faba3dae3faca109673dd5658eac32156f7207c
leo2n <ttxs.an@gmail.com>
2020-07-05 05:15:44 +0800

    fix:1800秒的idle会导致重复使用tcp连接的时候偶然性报错, 所以还是改为300miao吧

--- redis_config.go
+++ redis_config.go
@@ -8,7 +8,7 @@ import (
 // 定义redis
 var pool = &redis.Pool{
 	MaxIdle: 20000,
-	IdleTimeout: 1800 * time.Second,
+	IdleTimeout: 300 * time.Second,
 	Dial: func() (conn redis.Conn, err error) {
 		return redis.Dial(networkType, address, redis.DialPassword(passwd))
 	},
