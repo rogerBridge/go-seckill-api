6cb79a191d296c5835d7ce3f2c345a1568338312
leo2n <ttxs.an@gmail.com>
2020-07-26 14:14:52 +0800

    modify:limit product '10000' limit num from 2 to 1

--- global_var.go
+++ global_var.go
@@ -5,7 +5,7 @@ var (
 	address = "127.0.0.1:6379"
 	//limitNum = 2 // 限制每个用户可以购买的相同productId的商品的数量, limitNum 必须小于等于商品实际的库存
 	limitNumMap = map[string]int{
-		"10000": 2,
+		"10000": 1,
 		"10001": 1,
 		"10002": 1,
 	}
