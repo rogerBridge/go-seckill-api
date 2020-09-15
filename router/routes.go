4d31c0c159c58ee7f7bd6dfe7947d1a15b787a0f
leo2n <fenrmen@gmail.com>
2020-09-15 22:47:37 +0800

    change: beautify middleware

--- router/routes.go
+++ router/routes.go
@@ -8,34 +8,39 @@ import (
 )
 
 type Route struct {
-	Method     string
-	Pattern    string
-	Handler    fasthttp.RequestHandler
+	Method  string
+	Pattern string
+	Handler fasthttp.RequestHandler
+	Middle  func(handler fasthttp.RequestHandler) fasthttp.RequestHandler
 }
 
 var routes = make([]Route, 0)
 
-func register(method, pattern string, handler fasthttp.RequestHandler) {
-	routes = append(routes, Route{method, pattern, handler})
+func register(method, pattern string, handler fasthttp.RequestHandler, middle func(handler fasthttp.RequestHandler) fasthttp.RequestHandler) {
+	routes = append(routes, Route{method, pattern, handler, middle})
 }
 
 func init() {
-	register(fasthttp.MethodPost, "/syncGoodsLimit", controllers.SyncGoodsLimit)
-	register(fasthttp.MethodPost, "/goodsList", controllers.GoodsList)
-	register(fasthttp.MethodPost, "/syncGoodsFromMysql2Redis", controllers.SyncGoodsFromMysql2Redis)
-	register(fasthttp.MethodPost, "/syncGoodsFromRedis2Mysql", controllers.SyncGoodsFromRedis2Mysql)
-	register(fasthttp.MethodPost, "/buy", auth.MiddleAuth(controllers.Buy))
-	register(fasthttp.MethodPost, "/cancelBuy", controllers.CancelBuy)
-	register(fasthttp.MethodPost, "/login", controllers.Login)
-	register(fasthttp.MethodPost, "/logout", controllers.Logout)
-	register(fasthttp.MethodPost, "/register", controllers.Register)
+	register(fasthttp.MethodPost, "/syncGoodsLimit", controllers.SyncGoodsLimit, nil)
+	register(fasthttp.MethodPost, "/goodsList", controllers.GoodsList, nil)
+	register(fasthttp.MethodPost, "/syncGoodsFromMysql2Redis", controllers.SyncGoodsFromMysql2Redis, nil)
+	register(fasthttp.MethodPost, "/syncGoodsFromRedis2Mysql", controllers.SyncGoodsFromRedis2Mysql, nil)
+	register(fasthttp.MethodPost, "/buy", controllers.Buy, auth.MiddleAuth)
+	register(fasthttp.MethodPost, "/cancelBuy", controllers.CancelBuy, nil)
+	register(fasthttp.MethodPost, "/login", controllers.Login, nil)
+	register(fasthttp.MethodPost, "/logout", controllers.Logout, nil)
+	register(fasthttp.MethodPost, "/register", controllers.Register, nil)
 }
 
 //
-func ThisRouter() *router.Router{
+func ThisRouter() *router.Router {
 	r := router.New()
 	for _, route := range routes {
-		r.Handle(route.Method, route.Pattern, route.Handler)
+		if route.Middle != nil {
+			r.Handle(route.Method, route.Pattern, route.Middle(route.Handler))
+		} else {
+			r.Handle(route.Method, route.Pattern, route.Handler)
+		}
 	}
 	return r
 }
