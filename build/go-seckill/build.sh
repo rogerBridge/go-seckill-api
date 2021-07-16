52fecbf20de50ae8e232eaf18e10f0ac7b1e7297
leo2n <fenrmen@gmail.com>
2021-07-16 17:47:03 +0800

    delete unused code

--- build/go-seckill/build.sh
+++ build/go-seckill/build.sh
@@ -6,7 +6,7 @@ configDir=../../cmd/go-seckill/config/;
 cp -r $configDir $workdir;
 echo "cp -r config folder to workdir success"
 cmdpath=../../cmd/go-seckill;
-cd $cmdpath && go build -o go-seckill main.go && cp $cmdpath/go-seckill $workdir ;
+cd $cmdpath && go build -o go-seckill main.go && cp $cmdpath/go-seckill $workdir && rm $cmdpath/go-seckill;
 cd $workdir;
 echo "cp go-seckill binary to workdir success"
 
@@ -16,9 +16,3 @@ docker build -t $username/go-seckill:test .;
 
 rm -r $workdir/config/;
 rm $workdir/go-seckill;
-
-# docker run -d \
-#   --name go-seckill \
-#   -p 127.0.0.1:4000:4000 \
-#   --network=go-seckill-network \
-#   $username/go-seckill:test;
